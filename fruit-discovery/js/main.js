d958a9fde11374e7df81bd20ced5dff8b88a3955
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-09-26 10:10:47 -0700

    GIF Random favorite tool utilized, and functional

--- fruit-discovery/js/main.js
+++ fruit-discovery/js/main.js
@@ -1,5 +1,23 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getRandomGif)
+if (localStorage.length >= 1)
+{
+  let len = localStorage.length;
+console.log(localStorage.length)
+let checker = 0;
+for (let k in localStorage)
+{
+  if (checker >= len) {
+    console.log('stop')
+  } else {
+    console.log(localStorage[k])
+    addToTheList(localStorage[k], k)
+  }
+checker++
+}
+
+}
+
+document.querySelector("#gifB").addEventListener('click', getRandomGif)
 
 const box = document.getElementById("box")
 
@@ -11,18 +29,14 @@ function getRandomGif(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         
-        //deletes fav bar and title for each new request so old ones aren't hanging around.
+        let d = data.data;
+        //deletes fav bar and titlefor each new request so old ones aren't hanging around.
         if (box.children.length > 0) {
           let b0 = box.children[0]
           let b1 = box.children[1]
           b0.remove()
           b1.remove()
-     
           }
-       //   box.removeChild(box.childNodes[0])
-         // box.removeChild(box.childNodes[1])
-        
-
 
         console.log(data)
   let i = document.querySelector("img") 
@@ -50,9 +64,62 @@ function getRandomGif(){
           title.innerText = data.data.title;
         }
 
+        //event listener to add that gif to your favorites
+        favThis.addEventListener('click', addItToMyFavs)
+
+        function addItToMyFavs() {
+          let sImg = d.images.fixed_width_small.url
+          let id = d.id
+         localStorage.setItem(id, sImg)
+        addToTheList(sImg, "hey");
+        favThis.removeEventListener('click', addItToMyFavs)
+        }
+
+
+
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
 
+
+
+function addToTheList(url, alt) {
+  let favBox = document.querySelector('#favBox')
+ let newFavLi = document.createElement('li')
+ let newFavImg = document.createElement('img')
+ let newFavRemove = document.createElement('button')
+
+ //creating image data
+ newFavImg.src = url;
+ newFavImg.alt = alt;
+
+ //creating remove button data
+ newFavRemove.innerText = "Remove me"
+
+ //add to li
+newFavLi.appendChild(newFavImg)
+newFavLi.appendChild(newFavRemove)
+
+//add li to list
+favBox.appendChild(newFavLi)
+
+
+//remove the favorite eventlistner
+newFavRemove.addEventListener('click', deleteFromLocalStorage)
+
+}
+
+
+function deleteFromLocalStorage(e) {
+  console.log(e.target)
+  let localStorageKey = e.target.parentElement.children[0].src
+  let localStorageId = e.target.parentElement.children[0].alt
+  localStorage.removeItem(localStorageId)
+  let parentItem = e.target.parentElement;
+  parentItem.remove();
+
+  console.log(localStorageKey)
+
+}
